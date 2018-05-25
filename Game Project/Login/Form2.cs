c6ca72fc358cee8884812bb4e5e994841ce3be21
ArtAmmirati <34355879+ArtAmmirati@users.noreply.github.com>
2018-05-24 21:11:41 -0400

    First Run

---
+++ Game Project/Login/Form2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Login
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form3 BlackJack;
+            BlackJack = new Form3();
+            BlackJack.ShowDialog();
+
+          
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                comboBox1.Items.Add(textBox1.Text.ToString());
+
+                textBox1.Clear();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Game Project/Players List/PlayerRegestration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Players_List
+{
+    public partial class PlayerRegestration : Form
+    {
+        public PlayerRegestration()
+        {
+            InitializeComponent();
+        }
+        
+        PlayerTable playerTable = new PlayerTable();
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+
+            playerTable.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
